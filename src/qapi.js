28de9102491d32b5aad2a3d7357eb4f3edab216c
Nathan Pointer <nwpointer@gmail.com>
2021-01-21 23:10:28 -0800

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"600a78b6aae0d1001eea7c2d",
+				"600a78b6aae0d1001eea7c2f"
+			]
+		},
+		"600a78b6aae0d1001eea7c2d": {
+			"id": "600a78b6aae0d1001eea7c2d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"600a78b6aae0d1001eea7c2f": {
+			"id": "600a78b6aae0d1001eea7c2f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
